7ee2c4b7751f59786439cb273dbdae441530a049
patrick mugambi <patrick@kali.patrick>
2025-10-21 10:24:30 +0300

    first html project

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log('hello node.js world!')
+
+import { writeFileSync, readFileSync} from "node:fs";
+
+
+const users = [{ name: 'Adam Ondra', email: 'adam.ondra@climb.ing' }]
+const usersJson = JSON.stringify(users)
+writeFileSync("backend/users.json", usersJson)
+
+const readUsersJson = readFileSync("backend/users.json")
+const readUsers = JSON.parse(readUsersJson)
+console.log(readUsers)
